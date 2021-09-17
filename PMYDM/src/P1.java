f7f8d6de300b919a89311b77692e8cd06c3bd8bf
JaimeRmcf <jaime.mellado1998@gmail.com>
2021-09-17 16:48:58 +0200

    17/09/2021

--- PMYDM/src/P1.java
+++ PMYDM/src/P1.java
@@ -1,2 +1,15 @@
 public class P1 {
+        public static  void main(String[] args) {
+            String sNombre = "alb erto fernan dez san chez";
+            String arraySeccion [] = sNombre.split(" ");
+            for (int i=0; i < arraySeccion.length ; i++);
+            {
+                if(arraySeccion[i].length() %2 != 0)
+                {
+                    System.out.println(arraySeccion[i]);
+                }
+            }
+
+
+        }
 }
